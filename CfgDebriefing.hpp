01ebe9198425a21b7232e8c77bf7407cf8ee3831
Niner1309 <sven0215@hotmail.com>
2017-05-01 11:06:29 +0200

    first commit

---
+++ CfgDebriefing.hpp
@@ -0,0 +1,9 @@
+class tf47_core_whitelistSlotFail
+{
+  title = "Whitelist Fehler";
+  subtitle = "Du bist nicht auf der Whitelist für diesen Slot!";
+  description = "Bitte wähle einen anderen Slot!";
+  pictureBackground = "";
+  picture = "TF47CoreScripts\data\slotFail.paa";
+  pictureColor[] = {1,1,1,1}; 
+};
---
+++ CfgFunctions.hpp
@@ -0,0 +1,44 @@
+class tf47_core
+{
+    class tf47_core
+    {
+        file = "TF47CoreScripts";
+        class initDatabase {};
+        class initWhitelist {};
+        class initTicketsystem {};
+        class initPlayer {};
+        class init {};
+    };
+};
+
+class tf47_core_util
+{
+    class tf47_core_util
+    {
+        file = "TF47CoreScripts\functions\util";
+        class getMarkerType {};
+    };
+};
+
+class tf47_core_whitelist
+{
+    class tf47_core_whitelist
+    {
+        file = "TF47CoreScripts\functions\whitelist";
+        class registerWhitelist {};
+        class checkWhitelist {};
+        class checkSlotWhitelist {};
+    };
+};
+
+class tf47_core_ticketsystem
+{
+    class tf47_core_ticketsystem
+    {
+        file = "TF47CoreScripts\functions\ticketsystem";
+        class registerVehicle {};
+        class changeTickets {};
+        class trackVehicle {};
+        class insertTicketlog {};
+    };
+};
